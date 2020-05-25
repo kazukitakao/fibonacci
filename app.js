4be0f9d08dbb3e4fd2b8136d9c556eb2edb50124
kazuki takao <kazukitakao55@gmail.com>
2020-05-25 20:31:20 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+// フィボナッチ数列のアルゴリズム
+// n個の列があった場合(0,1を除く)、(n-1)+(n-2)
+function fib(n) {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    }
+    return fib(n - 1) + fib(n - 2);
+  }
+  
+  const length = 40;
+  for (let i = 0; i <= length; i++) {
+    console.log(fib(i));
+  }
